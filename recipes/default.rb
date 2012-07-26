45ce50c5e82b7943bb6ac16699dbc456d9aca85e
Dotan J. Nahum <jondotan@gmail.com>
2012-07-26 10:48:03 +0300

    adding coffeescript

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,9 @@
 include_recipe "nodejs::npm"
+
 execute "npm install forever --global" do
   not_if 'which forever'
+end
+
+execute "npm install coffee-script --global" do
+  not_if 'which coffee'
 end
