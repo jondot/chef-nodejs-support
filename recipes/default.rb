46f6e99f2090fb0819f0684c81aa7cea00cab3a4
Dotan Nahum <jondotan@gmail.com>
2013-03-18 14:17:14 +0200

    locking version

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,6 @@ execute "npm install forever --global" do
   not_if 'which forever'
 end
 
-execute "npm install coffee-script --global" do
+execute "npm install coffee-script@#{node['nodejs-support']['coffeescript_version']} --global" do
   not_if 'which coffee'
 end
